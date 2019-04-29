fce5dd90d573082b699e9f6853a0603bef4deaf4
Goksel Goktas <gokselgoktas@gmail.com>
2019-04-29 21:18:19 +0300

    Add is_string

--- hotline.lua
+++ hotline.lua
@@ -4,6 +4,10 @@ _.is_nil = function (value)
     return value == nil
 end
 
+_.is_string = function (value)
+    return type(value) == 'string'
+end
+
 _.is_table = function (value)
     return type(value) == 'table'
 end
